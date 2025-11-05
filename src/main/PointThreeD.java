201a6868b4df2a4c0b8937102fd21ad3228b608b
Connor DeLuca <cojode6@gmail.com>
2025-11-05 18:20:18 -0500

    PointThreeD completed

--- src/main/PointThreeD.java
+++ src/main/PointThreeD.java
@@ -11,7 +11,41 @@ public class PointThreeD {
 		zPoint = 0.0;
 	}
 	
-	public Poi
+	public PointThreeD(double x, double y, double z) {
+		xPoint = x;
+		yPoint = y;
+		zPoint = z;
+	}
+
+	@Override
+	public String toString() {
+		return "PointThreeD [xPoint=" + xPoint + ", yPoint=" + yPoint + ", zPoint=" + zPoint + "]";
+	}
+
+	public double getxPoint() {
+		return xPoint;
+	}
+
+	public void setxPoint(double xPoint) {
+		this.xPoint = xPoint;
+	}
+
+	public double getyPoint() {
+		return yPoint;
+	}
+
+	public void setyPoint(double yPoint) {
+		this.yPoint = yPoint;
+	}
+
+	public double getzPoint() {
+		return zPoint;
+	}
+
+	public void setzPoint(double zPoint) {
+		this.zPoint = zPoint;
+	}
 	
-	// keep making this according to the UML diagram
+	
+
 }
